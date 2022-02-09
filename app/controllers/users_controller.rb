edd48efaa033e78264b4f2dab7240c336c3fbec1
Muhammad Tayyab <Bcsf17a512@pucit.edu.pk>
2022-02-09 19:27:54 +0500

    signup-complete by adding create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,12 @@ class UsersController < ApplicationController
         @user= User.new
     end
     def create
+        @user=User.new(params.require(:user).permit(:username,:email,:password))
+        if @user.save
+            flash[:notice] ="hy #{@user.username} welcome to my app."
+            redirect_to articles_path
+        else
+            render 'new'
+        end        
     end
 end
