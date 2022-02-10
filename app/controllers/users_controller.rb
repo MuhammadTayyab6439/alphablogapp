3326299fb7c766b6398430a00e60b2cbd9de790f
Muhammad Tayyab <Bcsf17a512@pucit.edu.pk>
2022-02-10 14:10:35 +0500

    Userindexing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
 
+
+
     def show
         @user=User.find(params[:id])
         @articles = @user.articles
     end
 
-
+    def index
+        @users=User.all
+    end
 
     def new
         @user= User.new
