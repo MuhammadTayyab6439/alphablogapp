4a98928392cf5d688bb6abda0905fed5b7675624
Muhammad Tayyab <Bcsf17a512@pucit.edu.pk>
2022-02-11 10:58:17 +0500

    manage-controller-helper-methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in
+
+    def current_user
+        #get the user on base of session which is saved during creation of user in users_controller
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+      end
+    
+      def logged_in?
+        !!current_user #convert it into bolean
+      end
+
 end
